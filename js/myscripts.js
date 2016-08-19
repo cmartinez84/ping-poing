d9cbcc0fb7ac951f23b3a32971715be2eceab3c0
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-08-19 09:45:07 -0700

    wil reutrn string of numerical results to the console as an array

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,14 +1,21 @@
-var result;
+var results;
+var resultsArray= [];
 var numberInput;
 ///////back end/////////
-var isValid = function(numberInput){
-  if (isNaN(numberInput)){
+var isValid = function(input){
+  if (isNaN(input)){
     result = "your entry was not a number. please enter a number";
   }
   else(
-    numberInput = parseInt(numberInput)
+    numberInput = parseInt(input)
   )
 }
+var pingPong = function(input){
+  for(var i = 0; i <input; i++){
+    resultsArray.push(i);
+  };
+};
+
 
 
 
@@ -19,6 +26,8 @@ $(function(){
     event.preventDefault();
     numberInput = $("#numberInput").val();
     isValid(numberInput);
-    $("#pingPongResults").text(result);
+    pingPong(numberInput);
+    result = resultsArray.join("");
+    $("#pingPongResults").text(results);
   });
 });
