ce1d89625085c8ec09497f6798ebc15ab7cb1970
chris martinez <chris martinez>
2017-01-03 23:16:58 -0800

    set time out for displaying each ping pong ball without sound

--- js/myscripts.js
+++ js/myscripts.js
@@ -33,15 +33,18 @@ $(function(){
     pingPong(numberInput);
     }
     for(var i=0; i <resultsArray.length; i ++){
-      var blop = new Audio("sounds/blop.wav");
-      blop.play();
       var random1 = Math.floor(Math.random() * 99);
       var random2 = Math.floor(Math.random() * 99);
-      makeBalls(resultsArray[i], random1, random2, i);
-      $("#playAgain").show();
+      setTimeout(makeBalls, 100 * i, resultsArray[i], random1, random2, i);
+      setTimeout(function(){
+        $("#playAgain").show();
+      }, i * resultsArray.length)
     }
   });
+
 var makeBalls = function(ballValue, random1, random2, i){
+  var blop = new Audio("sounds/blop.wav");
+  blop.play();
   if(ballValue==="ping pong"){
     $("#pingPongResults").append("<p class='balls pingPongBall'>" + resultsArray[i] + "</p>");
     $("p:nth-child("+ (i+1) +")").css({"position":"fixed", "top": random1+"%","left":random2 +  "%"});
