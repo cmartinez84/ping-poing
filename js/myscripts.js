d507f89d84078efd58e890244e00e580ced80d70
chris martinez <chris martinez>
2017-01-03 23:02:53 -0800

    create makeBalls function, reusable for all cases, add blop sound

--- js/myscripts.js
+++ js/myscripts.js
@@ -24,7 +24,6 @@ $(function(){
   $("#getNumber").submit(function(event){
     event.preventDefault();
     numberInput = $("#numberInput").val();
-
     if(isNaN(numberInput) || (numberInput <=0)){
       $("form").addClass("has-error");
       $("#warning").show();
@@ -34,29 +33,31 @@ $(function(){
     pingPong(numberInput);
     }
     for(var i=0; i <resultsArray.length; i ++){
-      if(resultsArray[i]==="ping pong"){
-        $("#pingPongResults").append("<p class='balls pingPongBall'>" + resultsArray[i] + "</p>");
-        var random1 = Math.floor(Math.random() * 99);
-        var random2 = Math.floor(Math.random() * 99);
-        $("p:nth-child("+ (i+1) +")").css({"position":"fixed", "top": random1+"%","left":random2 +  "%"});
-      }
-      else if(resultsArray[i].length ===4){
-        $("#pingPongResults").append("<p class='balls pongBall'>" + resultsArray[i] + "</p>");
-        var random1 = Math.floor(Math.random() * 99);
-        var random2 = Math.floor(Math.random() * 99);
-        $("p:nth-child("+ (i+1) +")").css({"position":"fixed", "top": random1+"%","left":random2 +  "%"});
-
-      }
-      else {
-        $("#pingPongResults").append("<p class='balls numberBall'>" + resultsArray[i] + "</p>");
-        var random1 = Math.floor(Math.random() * 99);
-        var random2 = Math.floor(Math.random() * 99);
-        $("p:nth-child("+ (i+1) +")").css({"position":"fixed", "top": random1+"%","left":random2 +  "%"});
-      }
+      var blop = new Audio("sounds/blop.wav");
+      blop.play();
+      var random1 = Math.floor(Math.random() * 99);
+      var random2 = Math.floor(Math.random() * 99);
+      makeBalls(resultsArray[i], random1, random2, i);
       $("#playAgain").show();
-
     }
   });
+var makeBalls = function(ballValue, random1, random2, i){
+  if(ballValue==="ping pong"){
+    $("#pingPongResults").append("<p class='balls pingPongBall'>" + resultsArray[i] + "</p>");
+    $("p:nth-child("+ (i+1) +")").css({"position":"fixed", "top": random1+"%","left":random2 +  "%"});
+  }
+  else if(ballValue.length ===4){
+    $("#pingPongResults").append("<p class='balls pongBall'>" + resultsArray[i] + "</p>");
+    $("p:nth-child("+ (i+1) +")").css({"position":"fixed", "top": random1+"%","left":random2 +  "%"});
+
+  }
+  else {
+    $("#pingPongResults").append("<p class='balls numberBall'>" + resultsArray[i] + "</p>");
+    $("p:nth-child("+ (i+1) +")").css({"position":"fixed", "top": random1+"%","left":random2 +  "%"});
+  }
+}
+
+
 
   $("#playAgain").click(function(){
     numberInput = 0;
